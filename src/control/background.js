2936c658cf95eaf067b6e830d280b3426f24e61d
enchanted-sword <xlucas4956@gmail.com>
2024-02-25 00:19:57 -0400

    run perms page

--- src/control/background.js
+++ src/control/background.js
@@ -1,8 +1,5 @@
 browser.runtime.onInstalled.addListener(async () => {
-  /* browser.tabs.create({
+  browser.tabs.create({
     url: '../ui/permissions.html'
-  }); */
-  await browser.tabs.query({ url: '*://*.cohost.org/*' }).then(async tabs => {
-    tabs.forEach(tab => browser.tabs.reload(tab.id));
   });
 });
