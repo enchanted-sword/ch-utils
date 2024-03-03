347bd34c52cb2fbb38c53aff45acf7ac059c5e23
enchanted-sword <xlucas4956@gmail.com>
2024-03-03 15:48:02 -0700

    stuffs

--- src/scripts/popoverComments.js
+++ src/scripts/popoverComments.js
@@ -1,12 +1,20 @@
 import { postFunction } from './utils/mutation.js';
 
+const linkSelector = '.co-thread-footer a';
+
+const onLinkClick = event => {
+  event.preventDefault();
+  event.stopPropagation();
+};
 const addPopovers = posts => {
   for (const post of posts) {
-
+    post.querySelector(linkSelector).onclick = onLinkClick;
   }
-}
+};
 
 export const main = async () => {
+  if (['/', '#'].includes(location.pathname)) return;
+
   postFunction.start(addPopovers);
 };
 
