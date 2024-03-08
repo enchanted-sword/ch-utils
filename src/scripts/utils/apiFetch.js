24441663f92c55bc6f987baa0263a5f1fe8f15da
enchanted-sword <xlucas4956@gmail.com>
2024-03-08 06:48:06 -0400

    improve api methods

--- src/scripts/utils/apiFetch.js
+++ src/scripts/utils/apiFetch.js
@@ -11,27 +11,27 @@ const removeParams = obj => {
 };
 
 export const apiFetch = async (path, body = {}) => fetch(`https://cohost.org/api${path}${stringifyParams(body?.queryParams)}`, removeParams(body))
-  .then(response => response.json()).catch(() => {
+  .then(response => response.json().then(response => {
+    if (response.constructor.name === 'Array') return response.map(({ result }) => result.data);
+    else if ('result' in response) return response.result.data;
+    else return response;
+  })).catch(() => {
     console.error('apiFetch error: failed to fetch resource', `request url: https://cohost.org/api${path}${stringifyParams(body?.queryParams)}`, `request body: ${removeParams(body)}`);
     return null;
   });
 
 export const followState = async projectHandle => {
-  const arr = await apiFetch('/v1/trpc/projects.followingState', { method: 'GET', queryParams: { batch: 1, input: { 0: { projectHandle } } } });
-  return arr[0].result.data.readerToProject;
+  const { readerToProject } = await apiFetch('/v1/trpc/projects.followingState', { method: 'GET', queryParams: { input: { projectHandle } } });
+  return readerToProject;
 };
 
 export const activeProjectId = async () => {
-  const arr = await apiFetch('/v1/trpc/login.loggedIn', { method: 'GET', queryParams: { batch: 1, input: {} } });
-  return arr[0].result.data.projectId;
+  const { projectId } = await apiFetch('/v1/trpc/login.loggedIn');
+  return projectId;
 }
 
-export const singlePost = async (handle, postId) => {
-  const arr = await apiFetch('/v1/trpc/posts.singlePost', { method: 'GET', queryParams: { batch: 1, input: { 0: { handle, postId } } } });
-  return arr[0].result.data;
-};
+export const singlePost = async (handle, postId) => await apiFetch('/v1/trpc/posts.singlePost', { method: 'GET', queryParams: { input: { handle, postId } } });
 
-export const displayPrefs = async () => {
-  const { result } = await apiFetch('/v1/trpc/users.displayPrefs');
-  return result.data;
-}
+export const displayPrefs = async () => await apiFetch('/v1/trpc/users.displayPrefs');
+
+export const listEditedProjects = async () => await apiFetch('/v1/trpc/projects.listEditedProjects');
