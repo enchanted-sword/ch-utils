1fd90ba744b17a7cf705f6d79d10e0d300d568e4
enchanted-sword <xlucas4956@gmail.com>
2024-04-20 12:33:58 -0300

    remove unused component

--- src/control/background.js
+++ src/control/background.js
@@ -3,24 +3,3 @@ browser.runtime.onInstalled.addListener(async () => {
     url: '../ui/permissions.html'
   });
 });
-
-const dynamicDnr = async ({ removeRuleIds, newRules }) => {
-  console.log(removeRuleIds, newRules);
-  await browser.declarativeNetRequest.updateDynamicRules({
-    removeRuleIds,
-    addRules: newRules,
-  });
-}
-
-let connectionPort;
-
-const connected = p => {
-  connectionPort = p;
-  connectionPort.onMessage.addListener(m => {
-    if (m.action === 'dynamicDnr') dynamicDnr(m.data);
-  });
-}
-
-browser.runtime.onConnect.addListener(connected);
-
-browser.runtime.onSuspend.addListener(() => { console.log("Unloading."); });
