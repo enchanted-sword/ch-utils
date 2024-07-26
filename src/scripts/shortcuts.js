085e260c8b00b2bd2480144385f56b5f7bb976e6
enchanted-sword <xlucas4956@gmail.com>
2024-07-25 21:04:54 -0300

    pagination and post scrolling

---
+++ src/scripts/shortcuts.js
@@ -0,0 +1,40 @@
+const keyHandler = ({ ctrlKey, key }) => {
+  if (!(document.querySelector(':focus') === null)) return;
+
+  if (['j', 'k'].includes(key)) {
+    const posts = Array.from(document.querySelectorAll('.renderIfVisible:has([data-postid])'));
+    if (posts.length === 0) return;
+    let scrollTarget;
+    if (key === 'k') scrollTarget = posts.find(post => Math.floor(post.getBoundingClientRect().y) > 80);
+    else scrollTarget = posts.reverse().find(post => Math.ceil(post.getBoundingClientRect().y) < 80);
+    if (typeof scrollTarget === 'undefined') {
+      if (key === 'j') window.scroll(0, 0);
+      else return;
+    }
+    else scrollTarget.scrollIntoView();
+  } else if (['ArrowLeft', 'ArrowRight'].includes(key) && ctrlKey) {
+    const scrollButtons = document.querySelectorAll('.renderIfVisible ~ .mb-12 a:has(svg), .renderIfVisible ~ div .mb-12 a:has(svg)');
+    if (scrollButtons.length === 0) return;
+    console.log(scrollButtons);
+    if (key === 'ArrowRight') {
+      if (/\?page=[\d]+#*/.test(scrollButtons[0].href)) {
+        if ((scrollButtons.length === 1 && window.location.search === '?page=0')
+          || scrollButtons.length === 2) window.location = window.location.href.replace(/\?page=(\d)+#*/, (_,a) => `?page=${+a + 1}`);
+      } else {
+        if (scrollButtons.length === 1 && /skipPosts=0|skipPosts=20|'page=1#'/.test(scrollButtons[0].href)) window.location = scrollButtons[0].href;
+        else if (scrollButtons.length === 2) window.location = scrollButtons[1];
+      }
+    } else {
+      if (/\?page=[\d]+#*/.test(scrollButtons[0].href)) {
+        if ((scrollButtons.length === 1 && window.location.search !== '?page=0')
+          || scrollButtons.length === 2) window.location = window.location.href.replace(/\?page=(\d)+#*/, (_,a) => `?page=${+a - 1}`);
+      } else {
+        if (scrollButtons.length === 1 && !/skipPosts=0|skipPosts=20|'page=1#'/.test(scrollButtons[0].href)) window.location =  scrollButtons[0];
+        else if (scrollButtons.length === 2) window.location = scrollButtons[0];
+      }
+    }
+  }
+}
+
+export const main = async () => document.addEventListener('keydown', keyHandler);
+export const clean = async () => document.removeEventListener('keydown', keyHandler);
