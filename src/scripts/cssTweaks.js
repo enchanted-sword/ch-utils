8ef66b40263031417133086aadc5d34efeee334e
meadow wowperfect <meadow@wowperfect.net>
2024-08-06 13:15:51 -0400

    concept

---
+++ src/scripts/cssTweaks.js
@@ -0,0 +1,25 @@
+const commentInputResizeClass = '-chutils-tweaks--comment-input-resize'
+const hideChornerClass = '-chutils-tweaks--hide-chorner'
+const compactTlClass = '-chutils-tweaks--compact-tl'
+const betterProfilesClass = '-chutils-tweaks--better-profiles'
+const timestampClass = '-chutils-tweaks--timestamp'
+const hideNamesClass = '-chutils-tweaks--hide-names'
+const hideHandlesClass = '-chutils-tweaks--hide-handles'
+const stickySbClass = '-chutils-tweaks--sticky-sb'
+const hideHideClass = '-chutils-tweaks--hide-hide'
+const compactCommentClass = '-chutils-tweaks--compact-comment'
+const rainbowCommentClass = '-chutils-tweaks--rainbow-comment'
+const spaceEfficiencyClass = '-chutils-tweaks--space-efficiency'
+const lowercaseTagsClass = '-chutils-tweaks--lowercase-tags'
+const squareAvatarsClass = '-chutils-tweaks--square-avatars'
+const squareCornersClass = '-chutils-tweaks--square-corners'
+
+export async function main() {
+
+  $('main').addClass('.......')
+
+}
+
+export async function clean() {
+
+}
