1bebae2c978e786c5cc1a60cc8f1f2f6c3e124f4
enchanted-sword <xlucas4956@gmail.com>
2024-08-12 20:16:45 -0300

    fix content not being re-hideable

--- src/scripts/showNsfw.js
+++ src/scripts/showNsfw.js
@@ -2,26 +2,24 @@ import { getOptions } from './utils/jsTools.js';
 import { activeProject } from './utils/user.js';
 import { mutationManager } from './utils/mutation.js';
 
-let nsfwButtonQuery = 'article .co-18-plus + div button'
+const customAttribute = 'data-show-nsfw';
+let nsfwButtonQuery = `article > div > div:not([${customAttribute}]) .co-18-plus + div button`;
 
-const clickShowPost = elems => {
-  for (const elem of elems) {
-    if (elem.textContent === 'show post') {
-      elem.click();
-    }
-  }
-};
+const clickShowPost = elems => elems.map(elem => {
+  elem.closest('article > div > div').setAttribute(customAttribute, '');
+  if (elem.textContent === 'show post')  elem.click();
+});
 
 export const main = async () => {
-  let {handles} = await getOptions('showNsfw')
-  handles = handles.split(',').map(h => h.replaceAll(/[@ \n]/g, ''))
+  let { handles } = await getOptions('showNsfw')
+  handles = handles.split(',').map(h => h.replaceAll(/[@ \n]/g, ''));
+  const currentHandle = activeProject.handle;
 
-  const currentHandle = activeProject.handle
-
-  if (handles.includes(currentHandle)) {
-    mutationManager.start(nsfwButtonQuery, clickShowPost)
-  }
+  if (handles.includes(currentHandle)) mutationManager.start(nsfwButtonQuery, clickShowPost);
 };
 
-export const clean = async () => mutationManager.stop(clickShowPost);
+export const clean = async () => {
+  mutationManager.stop(clickShowPost);
+  $(`[${customAttribute}]`).removeAttr(customAttribute);
+}
 
