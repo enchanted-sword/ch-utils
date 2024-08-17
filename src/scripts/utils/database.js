dcbcc866bd166af684221ba421f6f6e912dd8346
enchanted-sword <xlucas4956@gmail.com>
2024-08-17 19:27:11 -0300

    docs

--- src/scripts/utils/database.js
+++ src/scripts/utils/database.js
@@ -21,8 +21,22 @@ const postData = ({ action, data, uuid = 0 }) => {
   connectionPort.postMessage({ action, data, uuid });
 };
 
+/**
+ * @param {object} data - object containing key-value pairs of object stores and data to enter into those stores
+ * @returns {void}
+ */
 export const cacheData = data => postData({ action: 'cache', data });
+
+/**
+ * @param {object} data - object containing key-value pairs of object stores and indices to delete from those stores
+ * @returns {void}
+ */
 export const clearData = data => postData({ action: 'clear', data });
+
+/**
+ * @param {object} data - object containing key-value pairs of object stores and indices to retrieve from those stores
+ * @returns {void}
+ */
 export const getData = data => new Promise(resolve => {
   const uuid = window.crypto.randomUUID();
   postData({ action: 'get', data, uuid });
@@ -35,5 +49,15 @@ const getIndexedResources = async (store, indices) => {
   const indexedResources = await getData(Object.fromEntries([[store, indices]]));
   return isArray ? indexedResources[store] : indexedResources[store][0];
 }
+
+/**
+ * @param {Number|Number[]} indices - single postId or array of postIds to fetch from the database
+ * @returns {object|object[]} post(s) - type of return matches type of input
+ */
 export const getIndexedPosts = indices => getIndexedResources('postStore', indices);
+
+/**
+ * @param {Number|Number[]} indices - single index (handle or projectId) or array of indices to fetch from the database
+ * @returns {object|object[]} project(s) - type of return matches type of input
+ */
 export const getIndexedProjects = indices => getIndexedResources('projectStore', indices);
