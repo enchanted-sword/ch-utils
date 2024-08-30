5cb925db42093a3b8454efbc27a0b36801683493
enchanted-sword <xlucas4956@gmail.com>
2024-08-30 06:35:31 -0300

    externalise buttons

--- src/scripts/utils/elements.js
+++ src/scripts/utils/elements.js
@@ -512,6 +512,63 @@ export const headerIconContainer = () => noact({
   className: 'ch-utils-headerIconContainer flex-1 flex items-center justify-end gap-3'
 });
 
+export const likeIcon = (toPostId, isLiked) => noact({
+  className: 'ch-utils-likeIcon w-6 h-6 pointer relative',
+  onclick: function() {
+    if (this.dataset.state) {
+      apiFetch('/v1/trpc/relationships.unlike', {
+        method: 'POST',
+        queryParams: { batch: 1 },
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify({ 0: { fromProjectId: activeProject.projectId, toPostId } })
+      }).then(this.dataset.state = '');
+    } else {
+      apiFetch('/v1/trpc/relationships.like?batch=1', {
+        method: 'POST',
+        queryParams: { batch: 1, },
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify({ 0: { fromProjectId: activeProject.projectId, toPostId } })
+      }).then(this.dataset.state = 'liked');
+    }
+  },
+  title: `like this post as ${activeProject.handle}`,
+  style: 'order:1',
+  dataset: { state: isLiked ? 'liked' : '' },
+  children: [{
+    className: 'w-6 h-6 pointer absolute top-0 left-0 co-action-button',
+    viewBox: '0 0 24 24',
+    fill: 'none',
+    stroke: 'currentColor',
+    'aria-hidden': true,
+    'stroke-width': 1.5,
+    children: [{
+      'stroke-linecap': 'round',
+      'stroke-linejoin': 'round',
+      d: 'M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z'
+    }]
+  }]
+});
+export const shareIcon = shareOfPostId => noact({
+  className: 'ch-utils-shareIcon',
+  href: `/${activeProject.handle}/post/compose?shareOfPostId=${shareOfPostId}`,
+  title: `share this post as ${activeProject.handle}`,
+  target: '_blank',
+  style: 'order:2',
+  children: [{
+    viewBox: '0 0 24 24',
+    className: 'h-6 w-6 co-action-button',
+    fill: 'none',
+    'stroke-width': 1.5,
+    stroke: 'currentColor',
+    'aria-hidden': 'true',
+    children: [{
+      'stroke-linecap': 'round',
+      'stroke-linejoin': 'round',
+      d: 'M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99'
+    }]
+  }]
+});
+
 const displayName = project => noact({
   className: 'co-project-display-name max-w-full flex-shrink truncate font-atkinson font-bold hover:underline',
   rel: 'author',
@@ -619,7 +676,7 @@ const threadFooter = post => noact({
         children: [{
           className: 'text-sm hover:underline',
           href: `${post.singlePostPageUrl}#comments`,
-          children: [`${post.numComments} comments`, post.numSharedComments ? ` + ${post.numSharedComments} on shared posts` : '']
+          children: [`${post.numComments} ${post.numComments === 1 ? 'comment' : 'comments'}`, post.numSharedComments ? ` + ${post.numSharedComments} on shared posts` : '']
         }]
       }
     ]
